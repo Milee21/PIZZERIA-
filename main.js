69af51fce9cf489d4ff30ba9267d81fa9103ba05
Milee21 <75302601+Milee21@users.noreply.github.com>
2021-04-01 15:41:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+document.getElementById("menup").style.display = "none";
+document.getElementById("maindiv").style.display = "inline-block";
+
+
+function hideshow()
+{
+    document.getElementById("maindiv").style.display = "none";
+    document.getElementById("menup").style.display = "inline-block";
+}
